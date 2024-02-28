f387ccfa443cd635bb58a2e2a484985a1d9af37d
Alex Guzhin <growfood1989@yandex.ru>
2024-02-28 14:44:39 +0300

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int account = 100;
-       int bonusScale = 100;
-       int amount = 1100;
-       int bonus;
-       if ( amount >= 1000) {
-           bonus = amount / bonusScale;
-       } else {
-           bonus = 0;
-       }
-       int finalaccount = account + amount + bonus;
+        int account = 100;
+        int bonusScale = 100;
+        int amount = 1100;
+        int bonus;
+        if (amount > 1000) {
+            bonus = amount / bonusScale;
+        } else {
+            bonus = 0;
+        }
+        int finalaccount = account + amount + bonus;
 
-       System.out.println(" Вы пополнили на " + amount + " руб. ");
-       System.out.println(" Ваш бонус составит " + bonus + " руб. ");
-       System.out.println(" Итого на счету " + finalaccount + " руб. ");
-       }
+        System.out.println(" Вы пополнили на " + amount + " руб. ");
+        System.out.println(" Ваш бонус составит " + bonus + " руб. ");
+        System.out.println(" Итого на счету " + finalaccount + " руб. ");
+    }
 }
